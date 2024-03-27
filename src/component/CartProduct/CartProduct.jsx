21278f14ec01f25d4a1f18cfe25e86825701854c
Md Rahed Mia <135752931+MD-Rahed-Mia@users.noreply.github.com>
2024-03-27 19:04:54 +0600

    added remove cart item

--- src/component/CartProduct/CartProduct.jsx
+++ src/component/CartProduct/CartProduct.jsx
@@ -1,63 +1,60 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './CartProduct.scss';
+import React, { useContext, useEffect, useState } from "react";
+import "./CartProduct.scss";
 
 import { RiDeleteBack2Line } from "react-icons/ri";
-import CartContext from '../../Context/CartContext';
-
-
+import CartContext from "../../Context/CartContext";
 
 export default function CartProduct() {
-
   const [product, setProduct] = useState([]);
 
   const { cart, setCart } = useContext(CartContext);
 
   useEffect(() => {
-    setProduct(cart)
-  }, [cart])
-
+    setProduct(cart);
+  }, [cart]);
 
   //increase quantity of cart
   function addQuantity(cartItem) {
-
     const updateCart = cart.map((item) => {
       if (item["product"]["id"] == cartItem["product"]["id"]) {
-        return { ...item, quantity: item.quantity + 1 }
+        return { ...item, quantity: item.quantity + 1 };
       } else {
         return item;
       }
-    })
+    });
     localStorage.setItem("cart", JSON.stringify(updateCart));
     setCart(updateCart);
   }
 
-
-
   //decrease quantity of cart
   function decQuantity(cartItem) {
-
     const updateCart = cart.map((item) => {
       if (item["product"]["id"] == cartItem["product"]["id"]) {
-
         if (item["quantity"] == 1) {
-          return { ...item, quantity: item.quantity }
+          return { ...item, quantity: item.quantity };
         }
 
-        return { ...item, quantity: item.quantity - 1 }
+        return { ...item, quantity: item.quantity - 1 };
       } else {
         return item;
       }
-    })
+    });
     //update new quantity to cart
     localStorage.setItem("cart", JSON.stringify(updateCart));
     setCart(updateCart);
   }
 
+  //update quantity after remove item
+  function deleteCartItem(cartItem) {
+    const updateCart = cart.filter((item) => {
+      return item["product"]["id"] != cartItem["product"]["id"];
+    });
+    setCart(updateCart);
+    localStorage.setItem("cart", JSON.stringify(updateCart));
+  }
 
   return (
-    <div className='cart-product-info'>
-
-
+    <div className="cart-product-info">
       <div className="cart-product-item">
         <table>
           <thead>
@@ -65,40 +62,51 @@ export default function CartProduct() {
               <th>Product</th>
               <th>Price</th>
               <th>Quantity</th>
-              <th>Sub Total</th></tr>
+              <th>Sub Total</th>
+            </tr>
           </thead>
           <tbody>
-
-            {
-              product && product.map((cartItem, index) => {
-                return (<tr key={index}>
-                  <td className='cart-product-details'>
-                    <div className='remove-cart-product'><RiDeleteBack2Line /></div>
-                    <div>
-
-                      <img src={cartItem["product"]["image"]} alt="" />
-                    </div>
-                    <div>
-                      <p>{cartItem["product"]["title"]}</p>
-                    </div>
-                  </td>
-                  <td className='cart-table-price'>${cartItem["product"]["price"]}</td>
-                  <td className='cart-table-quantity'>
-                    <button onClick={() => decQuantity(cartItem)}>-</button>
-                    <p>{cartItem.quantity}</p>
-                    <button onClick={() => addQuantity(cartItem)}>+</button>
-                  </td>
-                  <td>${cartItem["product"]["price"] * cartItem.quantity}</td>
-                </tr>)
-              })
-            }
+            {product &&
+              product.map((cartItem, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="cart-product-details">
+                      <div className="remove-cart-product">
+                        <RiDeleteBack2Line
+                          onClick={() => deleteCartItem(cartItem)}
+                        />
+                      </div>
+                      <div>
+                        <img src={cartItem["product"]["image"]} alt="" />
+                      </div>
+                      <div>
+                        <p>{cartItem["product"]["title"]}</p>
+                      </div>
+                    </td>
+                    <td className="cart-table-price">
+                      ${cartItem["product"]["price"]}
+                    </td>
+                    <td className="cart-table-quantity">
+                      <button onClick={() => decQuantity(cartItem)}>-</button>
+                      <p>{cartItem.quantity}</p>
+                      <button onClick={() => addQuantity(cartItem)}>+</button>
+                    </td>
+                    <td>${cartItem["product"]["price"] * cartItem.quantity}</td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
       </div>
 
       <div className="cart-coupon-item">
         <div>
-          <input type="text" name="coupon" id="coupon" placeholder='coupon code' />
+          <input
+            type="text"
+            name="coupon"
+            id="coupon"
+            placeholder="coupon code"
+          />
           <button>apply coupon</button>
         </div>
         <div>
@@ -106,5 +114,5 @@ export default function CartProduct() {
         </div>
       </div>
     </div>
-  )
+  );
 }
